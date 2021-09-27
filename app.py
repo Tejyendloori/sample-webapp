1b035b4883682452498d63587bb24f4f6bb8ab47
Tejyendloori <48048482+Tejyendloori@users.noreply.github.com>
2021-09-27 13:39:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def create_row_in_gs():
         #AmountUsed = request.json["AmountUsed"]
         #MembershipId = request.json["MembershipId"]
         MemberPhone = request.json["MemberPhone"]
-#         print("Received_Input")
+        print("Received_Input")
         #Sequence = request.json["Sequence"] 
         #BalanceType = request.json["BalanceType"] 
         #RecognitionId = request.json["RecognitionId"]
@@ -62,7 +62,7 @@ def create_row_in_gs():
         #     headers={'Content-Type': 'application/json'}
         # )
         # return response.content
-#         print(str('VCO'+str(MemberPhone)))
+        print(str('VCO'+str(MemberPhone)))
         return str('VCO'+str(MemberPhone))
         
         
